e03a183eaa1c04efb08fd9dc282c441fb86beb43
Lendemor <thomas.brandeho@gmail.com>
2024-04-19 15:09:25 +0200

    update template for better initial layout

--- blank_template/blank_template.py
+++ blank_template/blank_template.py
@@ -1,10 +1,26 @@
 """Welcome to Reflex! This file outlines the steps to create a basic app."""
+
 from rxconfig import config
 
 import reflex as rx
 
 docs_url = "https://reflex.dev/docs/getting-started/introduction"
 filename = f"{config.app_name}/{config.app_name}.py"
+
+color_button_style = {
+    "position": "fixed",
+    "right": "2em",
+    "top": "2em",
+    "background": "transparent",
+    "color": rx.color("gray", 12),
+}
+
+link_style = {
+    "border": "0.1em solid",
+    "padding": "0.5em",
+    "border_radius": "0.5em",
+    "_hover": {"color": rx.color("accent", 9)},
+}
 
 
 class State(rx.State):
@@ -15,28 +31,17 @@ class State(rx.State):
 
 def index() -> rx.Component:
     # Welcome Page (Index)
-    return rx.fragment(
-        rx.color_mode.button(rx.color_mode.icon(), float="right"),
+    return rx.container(
+        rx.color_mode.button(rx.color_mode.icon(), style=color_button_style),
         rx.vstack(
-            rx.heading("Welcome to Reflex!", font_size="2em"),
-            rx.box("Get started by editing ", rx.code(filename, font_size="1em")),
-            rx.link(
-                "Check out our docs!",
-                href=docs_url,
-                border="0.1em solid",
-                padding="0.5em",
-                border_radius="0.5em",
-                _hover={
-                    "color": rx.color_mode_cond(
-                        light="rgb(107,99,246)",
-                        dark="rgb(179, 175, 255)",
-                    )
-                },
-            ),
-            spacing="1",
-            font_size="2em",
-            padding_top="10%",
+            rx.heading("Welcome to Reflex!", size="9"),
+            rx.text("Get started by editing ", rx.code(filename), size="5"),
+            rx.link("Check out our docs!", href=docs_url, style=link_style),
+            spacing="5",
+            justify="center",
+            min_height="90vh",
         ),
+        rx.logo(),
     )
 
 
